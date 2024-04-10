4f247ae62a274f82d95975a2674289d9177529df
Donovan Barrientos <donovan_ba@outlook.com>
2024-04-10 12:36:02 -0600

    added modules and routes

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: ``
+})
+export class MenuComponent {
+  items: MenuItem[] = [];
+
+  ngOnInit(){
+    this.items = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+          },
+        ]
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+      }
+    ]
+  }
+}
